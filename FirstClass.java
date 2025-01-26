d735b8aa9c6e968b1b8383f3d95cc4cf5b426c5f
vanshbhandari31 <145548397+vanshbhandari31@users.noreply.github.com>
2025-01-26 16:38:40 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class FirstClass {
+    public static void main(String args[]) {
+        // System.out.println("hello world 123");
+        // System.out.println(2+3);
+        // System.out.println("hello world 123");
+        // String name = "tony stark";
+        // System.out.println(name);
+        int a = 10;
+        int b = 20;
+        int sum = a-b;
+        System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+        String name2 =sc.next();
+        System.out.println(name2);
+
+    }
+}
