1b592475a2c6d6ab264b24b475e43d2e73976fac
vanshbhandari31 <145548397+vanshbhandari31@users.noreply.github.com>
2025-01-28 01:09:43 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class tempCodeRunnerFile {
+    public static int calcfactorial(int num){
+        int fact=1;
+        for (int i = 1; i <= num ; i++) {
+            fact=fact*i;
+        }
+        return fact;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int num = sc.nextInt();
+        int fact=calcfactorial(num);
+        System.out.println(fact);
+    }
+    
+}
+ 
