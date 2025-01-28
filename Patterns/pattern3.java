b0463f96b7d0a274ffd35af537d7f117c1cd6409
vanshbhandari31 <145548397+vanshbhandari31@users.noreply.github.com>
2025-01-29 01:05:56 +0530

    Add files via upload

---
+++ Patterns/pattern3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+         int rows = sc.nextInt();
+        for(int i = 1 ; i<=rows ;i++){
+        for (int j = 0; j < i; j++) {
+            System.out.print("*  ");
+                       
+        }
+        System.out.println("");
+    }
+        
+    }
+    
+    
+}
