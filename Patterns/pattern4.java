a4d5238be4694afee4c68566171db8e3b19cc9b1
vanshbhandari31 <145548397+vanshbhandari31@users.noreply.github.com>
2025-01-31 00:41:55 +0530

    Add files via upload

---
+++ Patterns/pattern4.java
@@ -0,0 +1,16 @@
+package PATTERNS;
+
+import java.util.*;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        for(int i = rows ; i >=0 ; i--){
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*  ");
+            }
+            System.out.println("");
+        }
+    }
+}
