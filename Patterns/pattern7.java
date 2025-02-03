5373b727540d516fc3cffc68bf3b641a30a0f8c0
vanshbhandari31 <145548397+vanshbhandari31@users.noreply.github.com>
2025-02-04 01:36:57 +0530

    Add files via upload

---
+++ Patterns/pattern7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        for (int i = rows; i >=0; i--) {
+            for (int j = 1; j<=i ; j++) {
+                System.out.print(j+" ");               
+            }
+            System.out.println("");
+        }
+    }
+    
+}
