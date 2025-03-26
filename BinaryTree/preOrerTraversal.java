9118cdd2b03d59ebdca75f7d8473c9046f87e7ae
vanshbhandari31 <145548397+vanshbhandari31@users.noreply.github.com>
2025-03-27 00:01:50 +0530

    Add files via upload

---
+++ BinaryTree/preOrerTraversal.java
@@ -0,0 +1,44 @@
+package BinaryTree;
+
+public class preOrerTraversal {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BT{
+        static int idx=-1;
+        public static Node build(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=build(nodes);
+            newNode.right=build(nodes);
+            return newNode;
+        }
+        public static void preOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + "\t");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BT tree=new BT();
+        Node root= BT.build(nodes);
+        BT.preOrder(root);
+    }
+
+}
